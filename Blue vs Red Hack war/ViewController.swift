8ce92f070fe6991ce81f993954a7eb1d6fa8fd8b
Jansen Rondael <prondael9481@jh-mba158-21.d214.org>
2018-03-20 11:34:47 -0500

    Labels, Text, image added

---
+++ Blue vs Red Hack war/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blue vs Red Hack war
+//
+//  Created by Jansen Rondael on 3/19/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var RedHacker: UITextField!
+    
+    @IBOutlet weak var BlueHacker: UITextField!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+       
+    }
+
+   
+
+
+}
+
