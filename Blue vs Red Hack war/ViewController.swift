0d950cfa3038b4d4acc2d162f5fb1703acdb68e9
Jansen Rondael <prondael9481@jh-mba158-21.d214.org>
2018-04-06 11:21:29 -0500

    5 switch puzzle Win

--- Blue vs Red Hack war/ViewController.swift
+++ Blue vs Red Hack war/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var RedHacker: UITextField!
-    
+    @IBOutlet weak var RedLabel: UILabel!
+    @IBOutlet weak var BlueLabel: UILabel!
     @IBOutlet weak var BlueHacker: UITextField!
-    override func viewDidLoad() {
+    
+     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
+        if textField == RedHacker {
+            RedLabel.text = RedHacker.text
+             self.RedHacker.alpha = 0
+            self.RedLabel.alpha = 1
+            return true
+            
+        }else{
+            BlueLabel.text = BlueHacker.text
+            self.BlueHacker.alpha = 0
+            self.BlueLabel.alpha = 1
+        }
+        return true
+    
+    }
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
+        
        
     }
-
    
-
+    @IBAction func Button1(_ sender: UISwitch) {
+        if (sender.isOn == true) {
+            
+        }else{
+            
+        }
+    }
+    
+    @IBAction func Button2(_ sender: UISwitch) {
+    }
+    
+    @IBAction func Button3(_ sender: UISwitch) {
+    }
+    @IBAction func Button4(_ sender: UISwitch) {
+    }
+    
+    @IBAction func Button5(_ sender: UISwitch) {
+    }
+    
 
 }
 
